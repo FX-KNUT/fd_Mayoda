d33225a5996238cbd8151a510add121949d7d543
unknown <nicecarrot@gmail.com>
2022-01-17 15:48:18 +0900

    commit테스트중

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// git test code
+/*
+  ThkffkThkffk~~
+*/
