543c0cee173ced773430ee64ae389109bb688709
mingi <alsrl4769@naver.com>
2022-01-24 23:36:18 +0900

    202124TestAPI설정완료

--- backend/MayodaBE/src/main/java/com/fx/mayodabe/controller/TestAPIController.java
+++ backend/MayodaBE/src/main/java/com/fx/mayodabe/controller/TestAPIController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//컨트롤러 뮨
+//컨트롤러 문
 @RestController
 public class TestAPIController {
     // URL
