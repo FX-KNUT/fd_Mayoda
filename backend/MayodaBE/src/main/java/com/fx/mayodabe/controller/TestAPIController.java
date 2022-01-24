fb09c24e3f78f2842b0d0aa05347194e7c4afe11
mingi <alsrl4769@naver.com>
2022-01-24 23:16:06 +0900

    수정

--- backend/MayodaBE/src/main/java/com/fx/mayodabe/controller/TestAPIController.java
+++ backend/MayodaBE/src/main/java/com/fx/mayodabe/controller/TestAPIController.java
@@ -1,18 +1,22 @@
 package com.fx.mayodabe.controller;
+
 //import 문
 import com.fx.mayodabe.model.TestAPIModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//컨트롤러 뮨
+//컨트롤러 문
 @RestController
 public class TestAPIController {
+
     // URL
     @RequestMapping("/get/test1")
+
     // name 파라미터 받기
     public TestAPIModel todo(@RequestParam String name)
     {
+
         // 입력 받는 name 값 출력
         System.out.println(name);
 
