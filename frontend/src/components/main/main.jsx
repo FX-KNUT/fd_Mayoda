1d48bec66c3aafa5d9316bc5ce8e5417507e2078
areumH <dkfmal0109@gmail.com>
2022-02-07 20:48:56 +0900

    main commit

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -4,18 +4,22 @@ import "./main.css";
 const Main = () => {
   return (
     <main class="padding">
-      <div class='today'>
+      <div class="today">
+        <div>
           <span>TODAY'S BUCKET</span>
-          <div class='today_style'>
-              <p>예정된 일정이 없습니다.</p>
-          </div>
+        </div>
+        <div class="today_style">
+          <p>예정된 일정이 없습니다.</p>
+        </div>
       </div>
       <div class="size"></div>
       <div class="size"></div>
       <div class="done">
-        <span>DONE HISTORY</span>
-        <div class='done_style'>
-            <p>예정된 일정이 없습니다.</p>
+        <div>
+          <span>DONE HISTORY</span>
+        </div>
+        <div class="done_style">
+          <p>예정된 일정이 없습니다.</p>
         </div>
       </div>
     </main>
