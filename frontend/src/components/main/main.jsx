789bf6855035106b4e80f5295ca8c64b14515246
areumH <dkfmal0109@gmail.com>
2022-02-07 14:54:59 +0900

    main commit

---
+++ frontend/src/components/main/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+  return (
+    <div id = 'main' class = 'padding'>
+      
+        <div class = 'today'>
+            <span>TODAY'S BUCKETS</span> <br/>
+            <p>예정된 일정이 없습니다.</p>
+        </div>
+        <div class = 'size'></div>
+        <div class = 'size'></div>
+        <div class = 'done'>
+            <span>DONE HISTORY</span> <br/>
+            <p>예정된 일정이 없습니다.</p>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Main;
