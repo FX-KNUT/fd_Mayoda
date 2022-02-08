0e13a94f85e5827912cf4c78d0d124d17c8c9a60
Lee <znlspf123@naver.com>
2022-02-08 16:50:14 +0900

    leesoyun TEST API TESTING

---
+++ frontend/src/components/main/Todos/todos.jsx
@@ -0,0 +1,15 @@
+const Todos = () => {
+
+    return (
+        <div className="today">
+          <div>
+            <span>TODAY'S BUCKET</span>
+          </div>
+          <div className="today_style">
+            <p>예정된 일정이 없습니다.</p>
+          </div>
+        </div>
+    );
+}
+
+export default Todos;
