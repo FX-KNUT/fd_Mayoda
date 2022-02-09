fca897b5b07e1450ef96fd6d13319839b4067f38
Lee <znlspf123@naver.com>
2022-02-09 21:33:58 +0900

    component tutorial

--- frontend/src/components/main/Todos/todos.jsx
+++ frontend/src/components/main/Todos/todos.jsx
@@ -1,10 +1,17 @@
+import {TodoTitle} from "./TodoTitle/TodoTitle";
+
 const Todos = () => {
+
+  // todo가 있으면 todo를 뿌리고
+  // todo가 없으면 "예정된 일정이 없습니다."
+
+  // ternary operator
+  // 삼항 연산자
+  // console.log("hello world!") ? true : false;
 
     return (
         <div className="today">
-          <div>
-            <span>TODAY'S BUCKET</span>
-          </div>
+          <TodoTitle />
           <div className="today_style">
             <p>예정된 일정이 없습니다.</p>
           </div>
