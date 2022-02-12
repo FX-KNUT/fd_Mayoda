4685bd9dae31c8d5c1df6e52a128022401b27333
suin <suin10100@gmail.com>
2022-02-13 02:48:00 +0900

    basic modal&dim

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,54 +1,52 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Dim from "../module/dim/dim";
 import Modal from "../module/modal/modal";
 import "./header.css";
 
-
-
 const Header = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
 
-  const [showModal, setshowModal] = useState(false);
-
-   
   const icons = [
-    { id: "person", name: "person_outline", label: "Login"},
-    { id: "calendar", name: "calendar_today", label: "Calendar"},
-    { id: "noti", name: "notifications", label: "Notification"},
-    { id: "test", name: "test"},
+    { id: "person", name: "person_outline", label: "Login" },
+    { id: "calendar", name: "calendar_today", label: "Calendar" },
+    { id: "noti", name: "notifications", label: "Notification" },
+    { id: "test", name: "test" },
   ];
 
   const onIconClicked = (id) => {
     console.log(`클릭이벤트 발생! - ${id}`);
-    if(id = "test") {
-       setshowModal(true); 
-      }
-    };
+    if ((id = "test")) {
+      setIsShowModal(true);
+    }
+  };
 
   return (
     <>
-    {showModal&&<Dim setshowModal={setshowModal}><Modal></Modal></Dim>}
-    <header className="header_container">
-      <p className="header_logo"> MAYODA </p>
-      <p className="header_space"></p>
-      <div className="header_icon">
-        {icons.map((icon) => (
-          <span className="header_button" key={icon.id}>
-            <span
-              id={icon.id}
-              className="material-icons md-36"
-              draggable="false"
-              role="button"
-              onClick={() => onIconClicked(icon.id)}
-            >
-              {icon.name}
+      {isShowModal && (
+        <Dim setIsShowModal={setIsShowModal}>
+          <Modal></Modal>
+        </Dim>
+      )}
+      <header className="header_container">
+        <p className="header_logo"> MAYODA </p>
+        <p className="header_space"></p>
+        <div className="header_icon">
+          {icons.map((icon) => (
+            <span className="header_button" key={icon.id}>
+              <span
+                id={icon.id}
+                className="material-icons md-36"
+                draggable="false"
+                role="button"
+                onClick={() => onIconClicked(icon.id)}
+              >
+                {icon.name}
+              </span>
+              <span className="label_transform">{icon.label}</span>
             </span>
-            <span className="label_transform">
-              {icon.label}
-            </span>
-          </span>
-        ))}
-      </div>
-    </header>
+          ))}
+        </div>
+      </header>
     </>
   );
 };
--- frontend/src/components/module/modal/modal.jsx
+++ frontend/src/components/module/modal/modal.jsx
@@ -1,13 +1,7 @@
-import { Children } from "react";
 import "./modal.css";
 
-const Modal = ({children}) => {
-
-    return (
-        <div className="module_modal">
-            {children}
-        </div>
-    );
-}
+const Modal = ({ children }) => {
+  return <div className="module_modal">{children}</div>;
+};
 
 export default Modal;
