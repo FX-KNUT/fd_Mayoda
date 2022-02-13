52ca9223c0e06abf04d223f96af7a5635fa4c8e6
nicecarrot <nicecarrot@gmail.com>
2022-02-13 17:54:47 +0900

    오류 수정

--- frontend/src/components/theme/lang.jsx
+++ frontend/src/components/theme/lang.jsx
@@ -6,20 +6,19 @@ import { useTranslation } from 'react-i18next';
 
 const LangSelect = () => {
   const [lang, setlang] = useRecoilState(langState);
-  const { i18n, seti18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const onChangelang = (e) => {
-    setlang(seti18n.changeLanguage(e.target.value));
+    setlang(i18n.changeLanguage(e.target.value));
   };
 
   useEffect(() => {
     localStorage.setItem('langState', lang);
   }, [lang]);
 
-  /* select 를 통해서 변환하고 싶은 단어는 i18n을 통해서 적어야 함 */
   return (
     <div>
-      <h1>{i18n('title')}</h1>
+      <h1>{t('title')}</h1>
       <select name="selectlang" onChange={onChangelang}>
         <option value="en">ENGLISH</option>
         <option value="kr">한국어</option>
